ea15c81e3e674f0566f47d86a05de56afb97ebf8
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2023-12-31 16:35:34 +0900

    _

---
+++ 2023/script.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var aria_main = document.querySelector("main");
+    var aria1 = document.querySelector(".aria1");
+    for(var _i_ = 1;i++;i>0){
+        let date1 = new Date();
+        let date2 = new Date('2024/1/1 0:00:00');
+        let date3 = date2.getTime() - date1.getTime();
+        if(date3 == 0||date3 > 0){
+            aria1.remove();
+            _i_ = 0;
+            aria_main.innerHTML = `<h1>明けましておめでとう🎍</h1><h2>Happy new year</h2>`;
+            console.log("happy new year");
+        }
+        console.log(date3)
+        let sec = Math.floor(date3 / 1000 % 60);
+        let min = Math.floor(date3 / 1000 / 60 % 60);
+        let hour = Math.floor(date3 / 1000 / 60 / 60 % 24);
+        let day = Math.floor(date3 / 1000 / 60 / 60 / 24);
+        console.log(day);
+        console.log(hour);
+        console.log(min);
+        console.log(sec);
+    };
+};
+
