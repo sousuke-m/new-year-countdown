d379380f0df9ce25a51a1da26eda07746b6d17bd
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2023-12-31 18:17:21 +0900

    update

--- 2023/script.js
+++ 2023/script.js
@@ -1,25 +1,19 @@
-window.onload = function() {
-    var aria_main = document.querySelector("main");
-    var aria1 = document.querySelector(".aria1");
-    for(var i = 2;i > 1;i++){
-        let date1 = new Date();
-        let date2 = new Date('2024/1/1 0:00:00');
-        let date3 = date1.getTime() - date2.getTime();
-        console.log(date3);
-        if(date3 == 0||date3 > 0){
-            aria1.remove();
-            i = 0;
-            aria_main.innerHTML = `<h1>明けましておめでとう🎍</h1><h2>Happy new year</h2>`;
-            console.log("happy new year");
-        }
-        let sec = Math.floor(date3 / 1000 % 60);
-        let min = Math.floor(date3 / 1000 / 60 % 60);
-        let hour = Math.floor(date3 / 1000 / 60 / 60 % 24);
-        let day = Math.floor(date3 / 1000 / 60 / 60 / 24);
-        console.log(day);
-        console.log(hour);
-        console.log(min);
-        console.log(sec);
-    };
+var aria_main = document.querySelector(".main");
+var aria1 = document.querySelector(".aria1");
+for(var i = 2;i > 1;i++){
+    let date1 = new Date();
+    let date2 = new Date(2024,1,1);
+    let date3 = date2.getTime() - date1.getTime();
+    if(date3 == 0||date3 < 0){
+        aria1.remove();
+        i = 0;
+        aria_main.innerHTML = `<h1>明けましておめでとう🎍</h1><h2>Happy new year</h2>`;
+        console.log("happy new year");
+    }
+    let sec = date3 / 1000;
+    let min = date3 / (24 * 1000);
+    let hour = date3 / (24 * 60 * 1000);
+    let day = date3 / (24 * 60 * 60 * 1000);
+    aria1.innerHTML = `来年まで、あと${day}日${hour}時間${min}分${sec}秒`;
 };
 
